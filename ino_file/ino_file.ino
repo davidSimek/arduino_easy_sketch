c5b30a04a09923ceee53c8378193e82a66c7c1bc
davidSimek <david.simek@infis.cz>
2024-07-15 00:27:48 +0200

    try 7 segment display

--- ino_file/ino_file.ino
+++ ino_file/ino_file.ino
@@ -1,14 +1,54 @@
+#include <stdint.h>
 #ifndef DELETE_LIBS
 #include <Arduino.h>
 #endif
 
+#define A 2
+#define B 3
+#define C 4
+#define D 5
+#define E 6
+#define F 7
+#define G 8
+#define DP 9
+
+#define TOP A
+#define BOTTOM D
+#define MIDDLE G
+#define TOP_LEFT B
+#define TOP_RIGHT F
+#define BOTTOM_LEFT C
+#define BOTTOM_RIGHT E
+#define DOT DP
+
+void flash(uint8_t pin, uint16_t time);
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);  // Set the built-in LED pin as an output
+    pinMode(A, OUTPUT);
+    pinMode(B, OUTPUT);
+    pinMode(C, OUTPUT);
+    pinMode(D, OUTPUT);
+    pinMode(E, OUTPUT);
+    pinMode(F, OUTPUT);
+    pinMode(G, OUTPUT);
+    pinMode(DP, OUTPUT);
 }
 
+
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);  // Turn on the LED
-    delay(200);  // Wait for 1 second
-    digitalWrite(LED_BUILTIN, LOW);   // Turn off the LED
-    delay(1000);  // Wait for 1 second
+    flash(TOP, 100);
+    flash(TOP_LEFT, 100);
+    flash(MIDDLE, 100);
+    flash(BOTTOM_RIGHT, 100);
+    flash(BOTTOM, 100);
+    flash(BOTTOM_LEFT, 100);
+    flash(MIDDLE, 100);
+    flash(TOP_RIGHT, 100);
+}
+
+
+void flash(uint8_t pin, uint16_t time) {
+    digitalWrite(pin, 1);
+    delay(time);
+    digitalWrite(pin, 0);
 }
